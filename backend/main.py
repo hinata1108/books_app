2faeb00ab1920167af4437b3b9bd604638225d1e
hinata1108 <240561028@ccmailg.meijo-u.ac.jp>
2026-03-03 17:37:44 +0900

    screipingのルート変更

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from screiping import scrape_books
+from backend.screiping import scrape_books
 import uvicorn
 app=FastAPI()
 
